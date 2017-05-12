9ac5f4df0b363343237382644e4f32568c27629c
Carrie Ward <carriewardprojects@gmail.com>
2017-05-12 13:24:46 -0700

    Ruby source code

---
+++ Ruby_Basics.rb
@@ -0,0 +1,41 @@
+# Welcome to Ruby
+
+my_name = "Carrie"
+puts my_name
+
+last_name = "Ward"
+print last_name
+
+2.times {print "Coffee!"}
+
+print "Hello, Welcome to Ruby!"
+
+# keywords: Class / def / end
+class Dog
+  def initialize(name)
+    @name = name
+  end
+end
+
+# I'm on a diet
+buy_icecream = false
+print buy_icecream
+
+# Ruby uses strings that can be represented by "strings" or :strings
+friend = :timothy
+puts friend
+
+# Ruby uses underscores instead of commas for large numbers
+number = 1_000
+puts numbers
+dividend = 4/2
+puts dividend
+
+remainer = 10%8
+puts remainder
+
+is_raining = false
+print is_raining
+
+exponent = 4**2
+puts exponent
